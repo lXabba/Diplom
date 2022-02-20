bca099c982efdebfd9124417261c4e2c01f0e32b
lXabba <lll333ll@mail.ru>
2022-02-20 12:37:37 +0300

    New try

---
+++ Cars/Assets/Car controll/Controll.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+  
+public class SimpleCarController : MonoBehaviour {
+    public List<AxleInfo> axleInfos; // информация о каждой оси
+    public float maxMotorTorque; // максимальный крутящий момент
+    public float maxSteeringAngle; // максимальный угол поворота колес
+      
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+         
+        foreach (AxleInfo axleInfo in axleInfos) {
+            if (axleInfo.steering) {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor) {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+    }
+}
+[System.Serializable]
+public class AxleInfo {
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // присоединено ли колесо к мотору?
+    public bool steering; // поворачивает ли это колесо?
+}
--- Cars/Assets/Scripts/SimpleCarController.cs
+++ Cars/Assets/Scripts/SimpleCarController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimpleCarController : MonoBehaviour {
+public class Controll : MonoBehaviour {
 
 	public void GetInput()
 	{
