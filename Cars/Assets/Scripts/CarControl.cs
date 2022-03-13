e06342ef3edfa3f75536e41995243d50d07f6b61
lXabba <lll333ll@mail.ru>
2022-03-13 14:25:35 +0300

    Собрала машинку, избавилась от лишних папок

---
+++ Cars/Assets/Scripts/CarControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody>().centerOfMass = new Vector3 (0, -transform.localScale.y, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
